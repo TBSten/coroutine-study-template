a95d4c3df600dfa1c85615bfc83dc09ff3d82387
TBSten <programmingcafeteria@gmail.com>
2024-04-18 11:50:39 +0900

    compose navigationをセットアップ

--- app/src/main/java/me/tbsten/coroutinestudytemplate/MainActivity.kt
+++ app/src/main/java/me/tbsten/coroutinestudytemplate/MainActivity.kt
@@ -6,10 +6,11 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
 import me.tbsten.coroutinestudytemplate.ui.theme.CoroutinestudytemplateTheme
 
@@ -24,7 +25,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    AppNav()
                 }
             }
         }
@@ -32,17 +33,13 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun AppNav() {
+    val navController = rememberNavController()
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CoroutinestudytemplateTheme {
-        Greeting("Android")
+    NavHost(navController = navController, startDestination = "home") {
+        composable("home") {
+        }
+        composable("favorites") {
+        }
     }
 }
